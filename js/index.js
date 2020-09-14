f8b03ed5a0981d330218b85a921e87dec4f3dc24
nikolaichub <chub.mykola@ukr.net>
2020-09-14 16:04:22 +0300

    start section added

---
+++ js/index.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('working..');
+
+    const startButtonColors = {
+        1: 'start__button-item--green',
+        2: 'start__button-item--orange',
+        3: 'start__button-item--red',
+        4: 'start__button-item--blue'
+    }
+
+    const startButtonMain = document.getElementById('startButtonMain');
+    let startButtonCounter = 0;
+
+    startButtonMain.addEventListener('mouseenter', () => {
+
+        if (startButtonCounter < 4) {
+            startButtonCounter++;
+
+            console.log(startButtonColors[startButtonCounter]);
+            startButtonMain.classList.add(startButtonColors[startButtonCounter]);
+            startButtonMain.classList.remove(startButtonColors[startButtonCounter - 1]);
+
+        } else {
+            startButtonCounter = 1;
+            startButtonMain.classList.remove(startButtonColors[2],startButtonColors[3],startButtonColors[4]);
+            startButtonMain.classList.add(startButtonColors[startButtonCounter]);
+        }
+
+    });
+
+});
